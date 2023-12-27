4ce991b1718ef55a700a42db4884768e8a49855f
Паша Лозинский <GeNeytly@yandex.ru>
2023-12-27 17:22:18 +0300

    feature (core/models) add UserRelationMixin and Profile model

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -5,10 +5,12 @@ __all__ = (
     "DatabaseHelper",
     "User",
     "Post",
+    "Profile",
 )
 
 from core.models.base import Base
 from core.models.db_helper import db_helper, DatabaseHelper
 from core.models.post import Post
 from core.models.product import Product
+from core.models.profile import Profile
 from core.models.user import User
--- core/models/post.py
+++ core/models/post.py
@@ -1,11 +1,14 @@
-from sqlalchemy import String, Text, ForeignKey
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String, Text
 from sqlalchemy.orm import Mapped, mapped_column
 
 from core.models.base import Base
+from core.models.mixins import UserRelationMixin
 
 
-class Post(Base):
+class Post(UserRelationMixin, Base):
+    _user_back_populates = "posts"
+
     title: Mapped[str] = mapped_column(String(100), unique=False)
     body: Mapped[str] = mapped_column(Text, default="", server_default="")
-
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
