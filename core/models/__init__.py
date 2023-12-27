c0661cfda181f59758c7bc3bfd8a847b8737edbb
Паша Лозинский <GeNeytly@yandex.ru>
2023-12-27 15:05:34 +0300

    feature (core/models) create post model

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -4,9 +4,11 @@ __all__ = (
     "db_helper",
     "DatabaseHelper",
     "User",
+    "Post",
 )
 
 from core.models.base import Base
 from core.models.db_helper import db_helper, DatabaseHelper
+from core.models.post import Post
 from core.models.product import Product
 from core.models.user import User
